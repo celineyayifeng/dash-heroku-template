b3cfaef255f1dd554d7f52ad12490962598d63ea
Celine Feng <83599921+celineyayifeng@users.noreply.github.com>
2021-08-13 18:52:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -203,5 +203,5 @@ def createlocaltable(lcl):
     return table
     
 if __name__ == '__main__':
-    app.run_server(debug=True,port='8057',host='0.0.0.0')
+    app.run_server(debug=True,port='8050',host='0.0.0.0')
 
